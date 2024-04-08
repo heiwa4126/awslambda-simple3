63a85aae866cf0c2a3f913f378af320e5e328076
heiwa4126 <heiwa4126@gmail.com>
2024-04-08 14:20:17 +0900

    Update and Fix bugs

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  next()
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.contentType('text/plain');
-	res.send("Hello World!\n" + new Date().toISOString() + "\n")
-})
+app.get("/", (req, res) => {
+  res.contentType("text/plain");
+  res.send("Hello World!\n" + new Date().toISOString() + "\n");
+});
 
-exports.app = app
+exports.app = app;
--- utils/localserver.js
+++ utils/localserver.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-'use strict'
-const app = require('../app')
-const port = '8080';
-app.app.listen(port, () => {
-  console.log(`app start listening on port ${port}`)
-})
+"use strict";
+
+const { app } = require("../app");
+const port = "8080";
+
+app.listen(port, () => {
+  console.log(`app start listening on port ${port}`);
+});
